38afcd3d125baa5d83539b3618a63d86b96aec09
Nurul Muttaqin <taqin98@Nuruls-MacBook-Pro.local>
2021-03-24 16:34:22 +0700

    update native upload

--- application/libraries/pdf.php
+++ application/libraries/pdf.php
@@ -2,7 +2,7 @@
 class pdf {
 
     function __construct() {
-        include_once APPPATH . './third_party/fpdf/fpdf.php';
+        include_once APPPATH . '/third_party/fpdf/fpdf.php';
     }
 }
 ?>
